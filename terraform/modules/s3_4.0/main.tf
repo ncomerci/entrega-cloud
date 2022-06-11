b78caec03375512d2fd61d5f8dbd65070cb49881
Euge <epineiro@itba.edu.ar>
2022-06-11 12:27:34 -0300

    Add logs bucket

--- terraform/modules/s3_4.0/main.tf
+++ terraform/modules/s3_4.0/main.tf
@@ -45,3 +45,21 @@ resource "aws_s3_object" "this" {
     content_type  = each.value.content_type
     storage_class = try(each.value.tier, "STANDARD")
 }
+
+# 6 - Log bucket
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "mhs-logs-itba-cp-g1"
+}
+
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.log_bucket.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
