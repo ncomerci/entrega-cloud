564cc949177c5222efc15c4456e27cee16025e8e
eugepineiro <epineiro@itba.edu.ar>
2022-06-14 21:52:52 -0300

    Add Aurora Serverless

--- terraform/organization/aurora.tf
+++ terraform/organization/aurora.tf
@@ -1,51 +1,21 @@
-#module "aurora" {
-#  source = "../../"
-#  name   = "aurora"
-#
-#  # PostgreSQL
-#  engine = "aurora-postgresql"
-#
-#  # MySQL
-#  # engine = "aurora"
-#
-#  engine_mode           = "serverless"
-#  engine_version        = null
-#  replica_scale_enabled = false
-#  replica_count         = 0
-#
-#  backtrack_window = 10 # ignored in serverless
-#
-#  subnets             = data.aws_subnet_ids.all.ids
-#  vpc_id              = data.aws_vpc.default.id
-#  monitoring_interval = 60
-#  skip_final_snapshot = true
-#  instance_type       = "db.r4.large" # ignored for serverless
-#  apply_immediately   = true
-#  storage_encrypted   = true
-#
-#  # PostgreSQL
-#  db_parameter_group_name         = aws_db_parameter_group.aurora_db_postgresql10_parameter_group.id
-#  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.aurora_cluster_postgresql10_parameter_group.id
-#
-#  scaling_configuration = {
-#    auto_pause               = true
-#    min_capacity             = 2
-#    max_capacity             = 16
-#    seconds_until_auto_pause = 300
-#    timeout_action           = "ForceApplyCapacityChange"
-#  }
-#}
-#
-## PostgreSQL
-#resource "aws_db_parameter_group" "aurora_db_postgresql10_parameter_group" {
-#  name        = "test-postgresql10-parameter-group"
-#  family      = "aurora-postgresql10"
-#  description = "test-postgresql10-parameter-group"
-#}
-#
-#resource "aws_rds_cluster_parameter_group" "aurora_cluster_postgresql10_parameter_group" {
-#  name        = "test-postgresql10-cluster-parameter-group"
-#  family      = "aurora-postgresql10"
-#  description = "test-postgresql10-cluster-parameter-group"
-#}
-#
+resource "aws_rds_cluster" "this" {
+  cluster_identifier = "mhs-cp-g1"
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "13.6"
+  database_name      = "mhs_users_db"
+  master_username    = "mhs_users_db"
+  master_password    = "mhs_users_db"
+
+  #serverlessv2_scaling_configuration {
+  #  max_capacity = 1.0
+  #  min_capacity = 0.5
+  #}
+}
+
+resource "aws_rds_cluster_instance" "this" {
+  cluster_identifier = aws_rds_cluster.this.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.this.engine
+  engine_version     = aws_rds_cluster.this.engine_version
+}
