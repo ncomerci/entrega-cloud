6a484928693d9feb3f94cfdaa5191d45a86742cf
eugepineiro <epineiro@itba.edu.ar>
2022-06-18 18:43:08 -0300

    Add CW module

--- terraform/modules/cloudwatch/main.tf
+++ terraform/modules/cloudwatch/main.tf
@@ -1,5 +1,9 @@
-resource "aws_cloudwatch_metric_alarm" "apigwalarm" {
-    alarm_name          = "apigwalarm"
+resource "aws_sns_topic" "this" {
+    name                = "apigw-alarm-topic"
+}
+
+resource "aws_cloudwatch_metric_alarm" "this" {
+    alarm_name          = "apigw_alarm"
     comparison_operator = "GreaterThanThreshold"
     evaluation_periods  = "1"
     metric_name         = "Latency"
@@ -7,9 +11,9 @@ resource "aws_cloudwatch_metric_alarm" "apigwalarm" {
     period              = "300"
     statistic           = "Average"
     threshold           = "10000.0"
-    alarm_description   = ""
-    alarm_actions       = ["arn:aws:sns:us-east-1:865913489650:epineiro@itba.edu.ar"]
-    dimensions {
+    alarm_description   = "Monitoring API GW requests"
+    alarm_actions       = [aws_sns_topic.this.arn]
+    dimensions = {
         ApiName = var.service_to_monitor
         Stage = "production"
     }
