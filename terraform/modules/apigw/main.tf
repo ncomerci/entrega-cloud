e8e6b45285158422906d315b4e78c7a116b182ed
eugepineiro <epineiro@itba.edu.ar>
2022-06-19 19:02:00 -0300

    Delete old apigw

--- terraform/modules/apigw/main.tf
+++ terraform/modules/apigw/main.tf
@@ -4,38 +4,12 @@
 
 resource "aws_api_gateway_rest_api" "this" {
   # provider = aws.aws
-
   name        = "AWSAPIGateway-MHS"
   description = "MHS Api Gateway"
+
+  body           = "${data.template_file.apigw-openapi.rendered}"
 }
 
-resource "aws_api_gateway_resource" "this" {
-  # provider = aws.aws
-
-  path_part   = "resource"
-  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.this.id
-}
-
-resource "aws_api_gateway_method" "this" {
-  # provider = aws.aws
-
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.this.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "this" {
-  # provider = aws.aws
-
-  rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.this.id
-  http_method             = aws_api_gateway_method.this.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.invoke_arn
-}
 
 resource "aws_api_gateway_deployment" "this" {
   # provider = aws.aws
@@ -43,12 +17,7 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   triggers = {
-    redeployment = sha1(jsonencode([
-      # aws_api_gateway_rest_api.this.body,
-      aws_api_gateway_resource.this.id,
-      aws_api_gateway_method.this.id,
-      aws_api_gateway_integration.this.id,
-    ]))
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
   }
 
   lifecycle {
@@ -63,3 +32,10 @@ resource "aws_api_gateway_stage" "this" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   stage_name    = "production"
 }
+
+data "template_file" "apigw-openapi" {
+  template = var.template_file
+
+  vars = var.template_file_vars
+}
+
