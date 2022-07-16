06e392f5aac80a1ab62892cb7813d668b2f0a344
eugepineiro <epineiro@itba.edu.ar>
2022-07-16 16:58:07 -0300

    Delete Cloudfront country restriction

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -3,5 +3,5 @@
 # ---------------------------------------------------------------------------
 
 output "endpoint" {
-  value = "https://${aws_cognito_user_pool.this.domain}.auth.${var.aws_region_name}.amazoncognito.com/signup?response_type=code&client_id=${aws_cognito_user_pool_client.this.id}&redirect_uri=${one(aws_cognito_user_pool_client.this.callback_urls)}"
+  value = "https://${var.domain_name}.auth.${var.aws_region_name}.amazoncognito.com/signup?response_type=code&client_id=${aws_cognito_user_pool_client.this.id}&redirect_uri=${one(aws_cognito_user_pool_client.this.callback_urls)}"
 }
